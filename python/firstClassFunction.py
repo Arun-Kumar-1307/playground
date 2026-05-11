2d356f19a098e960599bdbc644a00d5b2b46e4a5
ak <arunkumar132004@gmail.com>
2026-05-11 17:38:14 +0000

    first class function

---
+++ python/firstClassFunction.py
@@ -0,0 +1,11 @@
+# print("Hello World")
+
+a = int(input("Enter a Number: "))
+
+def multiply_40():
+    return 40 * 10
+
+def divide(func1, num):
+    return func1 / num
+
+print("The result is {}".format(divide(multiply_40(),a)))
